f5c25825e74ec4f4934c9a77ab805bac0ed52b8e
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-17 18:51:50 +0530

    4th

---
+++ ClockDownTimer.py
@@ -0,0 +1,16 @@
+# ClockDown Timer.
+
+import time
+
+seconds = int(input("Enter seconds: "))
+
+while seconds > 0:
+    minutes = seconds // 60
+    sec = seconds % 60
+
+    print(f"{minutes:02d}:{sec:02d}")
+    time.sleep(1)
+
+    seconds -= 1
+
+print("Time's Up!")
